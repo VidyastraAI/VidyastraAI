940fba167e9bd5157348f56d769437335c81eb2f
RoyalDavid72 <bhardwajdavid6@gmail.com>
2026-06-08 12:02:07 +0530

    added teacher dashboard and update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import VidyastraLogin from './pages/loginpage.jsx';
 import ForgotPassword from './pages/forgot_password.jsx';
 import StudentDashboard from './pages/Student_dashboard.jsx';
+import TeacherDashboard from './pages/Teacher_dashboard.jsx';
 
 function App() {
   return (
@@ -12,12 +13,15 @@ function App() {
       <Routes>
         {/* The main login page */}
         <Route path="/" element={<VidyastraLogin />} />
-        
+
         {/* The forgot password page */}
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
-         {/* The Student dashboard page */}
-         <Route path="/student-dashboard" element={<StudentDashboard />} />
+        {/* The Student dashboard page */}
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+
+        {/* The Teacher dashboard page */}
+        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
       </Routes>
     </BrowserRouter>
   );
